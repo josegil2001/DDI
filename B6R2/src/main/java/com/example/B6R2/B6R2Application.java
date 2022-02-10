3cb753547ee04acab33b22ef3b000c5784b946c4
Jose <gil.fejos20@zaragoza.salesianos.edu>
2022-02-10 12:51:15 +0100

    B6R2

---
+++ B6R2/src/main/java/com/example/B6R2/B6R2Application.java
@@ -0,0 +1,13 @@
+package com.example.B6R2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B6R2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B6R2Application.class, args);
+	}
+
+}
---
+++ B6R2/src/test/java/com/example/B6R2/B6R2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.B6R2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B6R2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
