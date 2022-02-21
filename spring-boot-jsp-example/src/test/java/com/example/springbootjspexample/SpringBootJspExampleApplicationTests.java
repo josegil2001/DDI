dc43076ed9dace4ebd55fe99cdb251966466ded3
Jose <gil.fejos20@zaragoza.salesianos.edu>
2022-02-21 12:45:15 +0100

    B5R7

---
+++ spring-boot-jsp-example/src/test/java/com/example/springbootjspexample/SpringBootJspExampleApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springbootjspexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class SpringBootJspExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-boot-jsp-example/src/test/java/org/junit/jupiter/api/Test.java
@@ -0,0 +1,11 @@
+package org.junit.jupiter.api;
+import org.testng.annotations.Test;
+
+public class Test {
+	
+    @Test
+    public void test() {
+
+    }
+
+}
